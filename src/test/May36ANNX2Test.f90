cc04c94d07f0c60691c6456ce0e05559dc1a9f86
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2023-12-18 16:16:17 -0800

    renamed variable to make it clearer.

--- src/test/May36ANNX2Test.f90
+++ src/test/May36ANNX2Test.f90
@@ -16,7 +16,7 @@ program May36ANNX2Test
     implicit none
 
     ! Variables   
-    REAL, DIMENSION(1:5) :: Qsac_pr, Qexp_pr
+    REAL, DIMENSION(1:5) :: SMSCG_OP, Qexp_pr
     INTEGER, DIMENSION(1:5) :: mon
     INTEGER :: ave_t, currM, currY
 
@@ -25,12 +25,12 @@ program May36ANNX2Test
     DOUBLE PRECISION :: epsilon
 
     INTERFACE
-        FUNCTION Ann_X2(Qsac_prv0, Qsac_prv1, Qsac_prv2, Qsac_prv3, Qsac_prv4, &
+        FUNCTION Ann_X2(SMSCG_OP0, SMSCG_OP1, SMSCG_OP2, SMSCG_OP3, SMSCG_OP4, &
                 Qexp_prv0, Qexp_prv1, Qexp_prv2, Qexp_prv3, Qexp_prv4, &
                 mon0, mon1, mon2, mon3, mon4, &
                 ave_type, currMonth, currYear, BeginDay, EndDay)  RESULT (outputX2)
             IMPLICIT NONE
-            REAL, INTENT(IN) :: Qsac_prv0, Qsac_prv1, Qsac_prv2, Qsac_prv3, Qsac_prv4, &
+            REAL, INTENT(IN) :: SMSCG_OP0, SMSCG_OP1, SMSCG_OP2, SMSCG_OP3, SMSCG_OP4, &
                     Qexp_prv0, Qexp_prv1, Qexp_prv2, Qexp_prv3, Qexp_prv4
             integer, INTENT(IN) :: currMonth, currYear
             integer, optional, intent(in) :: BeginDay, EndDay
@@ -46,8 +46,8 @@ program May36ANNX2Test
     read (100, *) currY;
 
     read (100, *)                              ;
-    !consume ! Qsac_pr(1:5)
-    read (100, *) Qsac_pr
+    !consume ! SMSCG_OP(1:5)
+    read (100, *) SMSCG_OP
     read (100, *)                              ;
     !consume ! Qexp_pr(1:5)
     read (100, *) Qexp_pr
@@ -66,7 +66,7 @@ program May36ANNX2Test
     close(UNIT = 100)
 
     actual_val = Ann_X2(&
-            Qsac_pr(1), Qsac_pr(2), Qsac_pr(3), Qsac_pr(4), Qsac_pr(5), &
+            SMSCG_OP(1), SMSCG_OP(2), SMSCG_OP(3), SMSCG_OP(4), SMSCG_OP(5), &
             Qexp_pr(1), Qexp_pr(2), Qexp_pr(3), Qexp_pr(4), Qexp_pr(5), &
             mon(1), mon(2), mon(3), mon(4), mon(5), &
             ave_t, currM, currY)
@@ -75,7 +75,7 @@ program May36ANNX2Test
 
         ! log the call and the values:
         write(*, *) "Ann_X2("
-        write(*, *) Qsac_pr(1), Qsac_pr(2), Qsac_pr(3), Qsac_pr(4), Qsac_pr(5)
+        write(*, *) SMSCG_OP(1), SMSCG_OP(2), SMSCG_OP(3), SMSCG_OP(4), SMSCG_OP(5)
         write(*, *) Qexp_pr(1), Qexp_pr(2), Qexp_pr(3), Qexp_pr(4), Qexp_pr(5)
         write(*, *) mon(1), mon(2), mon(3), mon(4), mon(5)
         write(*, *) ave_t, currM, currY, ") = ", actual_val
